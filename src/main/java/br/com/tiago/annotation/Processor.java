3ecf6112955f0a982eea3eb7ecfa8de3cf7f1be3
Tiago Silva <tiago.jose@netshoes.com>
2017-04-24 19:28:34 -0300

    Cria anotação

---
+++ src/main/java/br/com/tiago/annotation/Processor.java
@@ -0,0 +1,19 @@
+package br.com.tiago.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Service;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+@Service
+public @interface Processor {
+
+    int sequence() default 0;
+    String value() default "";
+
+}
